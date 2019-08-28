3ae00a48ed2215ead5e544cd0b7deb34da7af098
lcdiao <120879566@qq.com>
2019-08-28 11:37:47 +0800

    使用nacos分布式配置中心实现修改配置文件项目自动修改
    在nacos上先配置好配置文件----service-consumer-config.yaml
    删除掉原来的application.yml配置文件，改为bootstrap.properties，在该文件配置，读取配置中心的配置文件，
    加上@RefreshScope注解，根据配置中心的配置文件动态刷新

--- hello-spring-cloud-alibaba-consumer/src/main/java/cn/lcdiao/spring/cloud/alibaba/consumer/controller/TestEchoController.java
+++ hello-spring-cloud-alibaba-consumer/src/main/java/cn/lcdiao/spring/cloud/alibaba/consumer/controller/TestEchoController.java
@@ -2,6 +2,8 @@ package cn.lcdiao.spring.cloud.alibaba.consumer.controller;
 
 import cn.lcdiao.spring.cloud.alibaba.consumer.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,16 @@ import javax.annotation.Resource;
  * @date 2019/8/28
  */
 
+//根据配置中心的配置文件动态刷新
+@RefreshScope
 @RestController
 public class TestEchoController {
 
     @Resource
     private EchoService echoService;
+
+    @Value("${user.name}")
+    private String username;
 
     @GetMapping(value = "/feign/echo/{str}")
     public String echo(@PathVariable("str") String str) {
@@ -28,5 +35,10 @@ public class TestEchoController {
     @GetMapping(value = "/lb")
     public String lb() {
         return echoService.lb();
+    }
+
+    @GetMapping(value = "/feign/echo")
+    public String echo() {
+        return echoService.echo(username);
     }
 }
